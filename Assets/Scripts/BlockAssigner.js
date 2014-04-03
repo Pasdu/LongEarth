97f6891489360fcc7d71f614a992eeb35cb84a60
Michael <mike@panickmedia.com>
2014-04-02 21:37:50 -0400

    First

---
+++ Assets/Scripts/BlockAssigner.js
@@ -0,0 +1,47 @@
+﻿/*blockAssigner
+	The block assigner initially assigned
+	a value to a textmesh to indicate which
+	type each block was but will likely be used
+	to develop tiles further after they've been
+	instantiated and coloured
+*/
+
+#pragma strict
+public var ui : uiClass;
+var type : int;
+function Awake () {
+	ui = GameObject.FindGameObjectWithTag("MainCamera").GetComponent(uiClass);
+}
+
+function Update () {
+
+}
+
+public function assignBlock(val : int){
+	type = val;
+}
+
+function OnMouseDown( ){
+	//sDebug.Log("Got a click");
+	var target : String;
+	switch(type)
+	{
+		case 0 :
+			target = "Water";
+			break;
+		case 1 :
+			target = "Plains";
+			break;
+		case 2 :
+			target = "Forest";
+			break;
+		case 3 :
+			target = "Quarry";
+			break;
+		case 4 :
+			target = "Settlement";
+			break;
+
+	}
+	ui.setElement(2, target);
+}
